0649f2f8d0f0fdf889e809c226b651bb9baa81d8
Alain <alain.bonnefond03@orange.fr>
2025-05-12 00:30:33 +0200

    first success in drawing a part of a spritesheet at the screen

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 -- Initialisation
 
---local map = require ("map.lua")
+require 'map'
 
 
 local position
 local positiona
+local source = love.graphics.newImage ("graphics/map/GRASS+.png")
+
 
 -- setting up the window size
 local width = 1920
@@ -15,29 +17,18 @@ local resolution = love.window.setMode( width, height)
 local test
 local test2
 
-partimage()
-
 --function map.creation()
 --end
-
-function partimage( source, tilenumber )
-    local i = tilenumber
-    local tile = {}
-    for i = tilenumber*16, ((tilenumber+1)*16-1), 1 do
-
-        for j = tilenumber*16, ((tilenumber+1)*16-1), 1 do
-           tile [i][j] = source[i][j]
-        end
-
-    end
-
-    return tile
-end
 
 function love.load()
     position = 0
     positiona = 300
-    test2 = partimage(love.graphics.newImage("graphics/map/GRASS+.png"), 1)
+    
+    if source ~= nil
+    then 
+        test2 = love.graphics.newQuad(0, 0, 16, 16, source:getDimensions())
+    end
+    --test2 = Partimage(love.graphics.newImage("graphics/map/GRASS+.png"), 1)
 end
 
 function love.update(dt) -- used to update the variable
@@ -51,5 +42,6 @@ end
 function love.draw() -- used to refresh the graphics based on the variable
     love.graphics.print('Hello World!', position, 300)
     love.graphics.print('Hello World!', positiona, 300)
-    love.graphics.draw(test2, position, height/2)
+    love.graphics.draw(source, test2, position, height/2)
 end
+
--- map.lua
+++ map.lua
@@ -1,1 +1,35 @@
 -- helper file for main.lua
+
+Partimage = function ( source, tilenumber )
+
+    local tile= {}
+    for i = tilenumber*16, (tilenumber+1)*16-1, 1 do
+        tile[i] = {}
+        for j = tilenumber*16, (tilenumber+1)*16-1, 1 do
+            if source[i] and source[i][j] then
+                love.graphics.NewQuad(i, j, 16, 16, source:getDimensions())
+            end
+        end
+
+    end
+
+    return tile
+end
+
+-- tilewidith and tileheight are the size of the each individual sprite in the sprite sheet
+function GenerateSprite (fullsheet, tilewidth, tileheight)
+    local fullsheetWidth = fullsheet:getWidth() / tilewidth -- give the number of sprite in x absis
+    local fullsheetHeight = fullsheet:getHeight() / tileheight -- give the number of sprite in y absis
+    
+    local tilenumber = 0
+    local tile = {}
+    for i = 0, fullsheetHeight - 1 do
+
+        for j = 0, fullsheetWidth - 1 do 
+            tile[tilenumber] = love.graphics.NewQuad(i * tilewidth, j * tileheight, tilewidth, tileheight, fullsheet:getDimensions())
+        end
+        
+    end
+
+    return tile
+end
